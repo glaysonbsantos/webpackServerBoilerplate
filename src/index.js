aa4ef5ca89e6950997ce271a3b13666febf1c327
Bê Santos <glaysonbss@gmail.com>
2022-02-24 18:22:52 -0300

    boilerplate updated - insertion of corejs and regenerator-runtime

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import Spaceship from "./Spaceship"
-import SpaceshipEngine from "./SpaceshipEngine"
+import 'core-js'
+import 'regenerator-runtime/runtime'
 
-const sophia = new Spaceship('Sophia', 10, 5)
-const amsterdan = new Spaceship('Amsterdã', 14, 10)
-const dwarfStar = new Spaceship('Estrela-Anã', 20, 4)
+function formatDate() { 
+    const date = new Date(); 
+    const formatDate = date.toLocaleString(); 
+    return formatDate; 
+}
 
-const sophiaEngine = new SpaceshipEngine(sophia)
-const amsterdanEngine = new SpaceshipEngine(amsterdan)
-const dwarfStarEngine = new SpaceshipEngine(dwarfStar)
-
-sophiaEngine.turnOn()
-amsterdanEngine.turnOn()
-dwarfStarEngine.turnOn()
-
-console.log('Promises')
+formatDate()
